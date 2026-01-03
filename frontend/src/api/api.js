2f9b59356d94e852f01a4b4359e6600ce306a9cd
flegodevteam <flegodevteam@gmail.com>
2026-01-03 21:42:15 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://service-center-app-backend-2ulqs.ondigitalocean.app/api";
+export const API_URL = "https://service-center-xxsk.vercel.app/api";
+// export const API_URL = "https://service-center-app-backend-2ulqs.ondigitalocean.app/api";
